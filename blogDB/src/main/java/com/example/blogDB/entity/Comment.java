430be8139f8b810e51642913c9c6fcc21db10965
russellng6 <rng@albany.edu>
2018-10-31 18:50:43 -0400

    Add Reader and Comment classes
    Added initial Reader.java and Comment.java implementation to the "entity" package

---
+++ blogDB/src/main/java/com/example/blogDB/entity/Comment.java
@@ -0,0 +1,61 @@
+package com.example.blogDB.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.FetchType;
+
+
+@Entity
+public class Comment {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;					//ID field
+	public Long getId() { 				//getter
+		return id; 
+	}
+    public void setId(Long id) { 		//setter
+    	this.id = id; 
+    }
+
+    private String content;				//content field
+    public String getContent() {
+    	return content;
+    }
+    public void setContent(String content) {
+    	this.content = content;
+    }
+
+   /* @Temporal(TemporalType.DATE)
+    Date publicationDate;
+    }*/
+    
+    public Comment() {}	//default  constructor
+    
+    public Comment(String content, Reader author) {
+    	this.content = content;
+    	//this.author = author;
+    	
+    }
+    
+    /*public String toString() {
+    	return "Commented by: " " +  getAuthor() + "\n" + getContent();
+    }*/
+
+    
+    /*@ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "comment_author")
+    private Reader author;
+    
+    public Reader getAuthor() {
+    	return author;
+    }*/
+
+
+}
---
+++ blogDB/src/main/java/com/example/blogDB/entity/Reader.java
@@ -0,0 +1,73 @@
+package com.example.blogDB.entity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.CascadeType;
+import java.util.List;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+@Entity
+public class Reader {
+
+	private Long id;					//ID field
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public Long getId() { 				//getter
+		return id; 
+	}
+    public void setId(Long id) { 		//setter
+    	this.id = id; 
+    }
+
+    private String name;				//name field
+    public String getName() { 		//getter
+		return name; 
+	}
+    public void setName(String name) { 	//setter
+    	this.name = name; 
+    }
+
+    private String password;			//password field
+    public String getPassword() { 		//getter
+		return password; 
+	}
+    public void setPassword(String password) { //setter
+    	this.password = password; 
+    }
+
+    private String email;				//email field
+    public String getEmail() { 		//getter
+		return email; 
+	}
+    public void setEmail(String email) { 	//setter
+    	this.email = email; 
+    }
+
+    protected Reader() {}	//default constructor, for JPA
+    
+    public Reader(String name, String password, String email){		//constructor
+    	this.name = name;
+    	this.password = password;
+    	this.email = email;
+    }
+
+    public String toString() {
+    	return "Reader:" + getName();
+    }
+    
+    /*
+    private Set<Comment> comments = new HashSet<>();
+    //set up relationships
+    @OneToMany(targetEntity=Comment.class, mappedBy = "reader", cascade = CascadeType.REMOVE, orphanRemoval = true)	//we can remove comments by deleting them from this arrayList
+	public Set<Comment> getComments() {
+		return comments;
+	}
+	public void setPosts(Set<Comment> comments) {
+		this.comments = comments;
+	}*/
+}
